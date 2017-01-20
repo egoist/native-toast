891ceb5a419fb99706cca5eab91caf17abc89ea2
EGOIST <0x142857@gmail.com>
2017-01-20 22:41:34 +0800

    refactor

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # native-toast [![NPM version](https://img.shields.io/npm/v/native-toast.svg)](https://npmjs.com/package/native-toast) [![NPM downloads](https://img.shields.io/npm/dm/native-toast.svg)](https://npmjs.com/package/native-toast)
 
-> Native-like toast notification but for the web. (JS + CSS < 2KB)
+> Native-like toast notification but for the web. (JS + CSS ≈ 4KB)
 
 ## Install
 
@@ -8,7 +8,7 @@
 $ npm install --save native-toast
 ```
 
-NPMCDN: https://unpkg.com/native-toast/dist/native-toast.umd.min.js
+NPMCDN: https://unpkg.com/native-toast/dist/
 
 ## Usage
 
@@ -17,17 +17,17 @@ First import `native-toast/dist/native-toast.css`, then:
 ```js
 const nativeToast = require('native-toast')
 
-nativeToast('hey yo!')
-
-nativeToast('hey yo', 'top')
-
 // specific a longer timeout
 nativeToast({
   message: 'wait wait!',
   position: 'top',
-  timeout: 5000
+  timeout: 5000,
+  type: 'warning'
 })
+// or nativeToast.warning(options)
 ```
+
+Four types: `success` `warning` `info` `error`
 
 ## API
 
@@ -62,6 +62,15 @@ Type: `number`<br>
 Default: `3000`
 
 Toast timeout, hide toast in specific timeout.
+
+##### type:
+
+Type: `string`<br>
+Default: `undefined`
+
+One of `success` `warning` `info` `error`.
+
+A short-hand to set type: `nativeToast.success(opts)` `nativeToast.error(opts)` and such.
 
 ## License
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
+import objectAssign from 'object-assign'
+
 let prevToast = null
+
+const icons = {
+  warning: `<svg viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6.25%"><path d="M8 17 C8 12 9 6 16 6 23 6 24 12 24 17 24 22 27 25 27 25 L5 25 C5 25 8 22 8 17 Z M20 25 C20 25 20 29 16 29 12 29 12 25 12 25 M16 3 L16 6" /></svg>`,
+  success: `<svg viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6.25%"><path d="M2 20 L12 28 30 4" /></svg>`,
+  info: `<svg viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6.25%"><path d="M16 14 L16 23 M16 8 L16 10" /><circle cx="16" cy="16" r="14" /></svg>`,
+  error: `<svg viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6.25%"><path d="M16 3 L30 29 2 29 Z M16 11 L16 19 M16 23 L16 25" /></svg>`
+}
 
 class Toast {
   constructor({
@@ -6,7 +15,9 @@ class Toast {
     position = 'bottom',
     timeout = 3000,
     el = document.body,
-    square = false
+    square = false,
+    type = '',
+    debug = false
   } = {}) {
     if (prevToast) {
       prevToast.destroy()
@@ -19,6 +30,12 @@ class Toast {
 
     this.toast = document.createElement('div')
     this.toast.className = `native-toast native-toast-${this.position}`
+
+    if (type) {
+      this.toast.className += ` native-toast-${type}`
+      this.message = `<span class="native-toast-icon-${type}">${icons[type] || ''}</span>${this.message}`
+    }
+
     this.toast.innerHTML = this.message
 
     if (square) {
@@ -30,7 +47,9 @@ class Toast {
     prevToast = this
 
     this.show()
-    this.hide()
+    if (!debug) {
+      this.hide()
+    }
   }
 
   show() {
@@ -56,15 +75,13 @@ class Toast {
   }
 }
 
-function toast(message, position, timeout) {
-  if (typeof message === 'string') {
-    return new Toast({
-      message,
-      position,
-      timeout
-    })
-  } else {
-    return new Toast(message)
+function toast(options) {
+  return new Toast(options)
+}
+
+for (const type of ['success', 'info', 'warning', 'error']) {
+  toast[type] = options => {
+    return toast({type, ...options})
   }
 }
 
